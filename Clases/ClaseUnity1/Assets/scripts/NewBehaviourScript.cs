b6df7ab3497a627234e82d22f55e803c51cd16d4
Rodrigo <rrdelalamo@gmail.com>
2019-12-12 22:12:26 -0500

    Saltando

--- Clases/ClaseUnity1/Assets/scripts/NewBehaviourScript.cs
+++ Clases/ClaseUnity1/Assets/scripts/NewBehaviourScript.cs
@@ -10,6 +10,8 @@ public class NewBehaviourScript : MonoBehaviour
     public float speed = 1f;
     public Animator animator;
 
+    public bool grounded { get { return RoundAbsoluteToZero(rigidbody2D.velocity.y) == 0; } }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +25,10 @@ public class NewBehaviourScript : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
         
         animator.SetBool("walk",(h!=0));
+        animator.SetBool("jump", !grounded);
+
+
+
 
         if(h > 0)
         {
@@ -35,12 +41,28 @@ public class NewBehaviourScript : MonoBehaviour
 
 
         transform.Translate(Vector3.right * h * speed);
+
+
+        if(grounded && Input.GetKeyDown(KeyCode.Space)) {
+            rigidbody2D.AddForce(Vector2.up * 4,ForceMode2D.Impulse);
+        }
+
+
     }
 
+    float RoundAbsoluteToZero(float decimalValue) {
+        decimalValue = Mathf.Abs(decimalValue);
+        if(decimalValue <= 0.01f) {
+            decimalValue = 0f;
+        }
+        return decimalValue;
+    }
+
+    /*
     void FixedUpdate() {
         if(Input.GetKeyDown(KeyCode.Space)) {
             rigidbody2D.AddForce(Vector2.up * 2, ForceMode2D.Impulse);
         }
-    }
+    }*/
 
 }
